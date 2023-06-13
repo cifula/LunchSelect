b1a502b1f69911bc21b8c688f97ff6e911827bdd
cifula <lidel95@gmail.com>
2023-06-13 09:28:42 +0900

    Feat: 팀프로젝트 이전

---
+++ front_lunch_selecter/src/Config/URL/URL.js
@@ -0,0 +1,4 @@
+export const backEndURL = "http://ec2-3-39-7-194.ap-northeast-2.compute.amazonaws.com:8080";
+export const frontEndURL = "http://lunchselect.s3-website.ap-northeast-2.amazonaws.com";
+// export const backEndURL = "http://localhost:8080";
+// export const frontEndURL = "http://localhost:3000";
